cdc89ad8c99610b00c6a0585f7fd9d5c2ee259b6
Adrian Benson <adrianbenson@Adrians-MacBook-Pro.local>
2018-04-29 18:07:03 -0400

    added basic styling. added calls for film, ship and character data

--- app.js
+++ app.js
@@ -1,27 +1,63 @@
+
+
 const movies = document.querySelector("#movieList");
+const ships = document.querySelector("#shipList");
+const characters = document.querySelector("#characterList");
 
 let movieData;
 let shipData;
 let characterData;
 let planetData;
+const SWAPI_URL = "https://swapi.co/api"
 
-const createLI = (text) => {
+const createLI = (list, text) => {
     const newLI = document.createElement("li");
     newLI.appendChild(document.createTextNode(text));
-    movies.appendChild(newLI);
+    list.appendChild(newLI);
+}
+
+const fetchMovies = () => {
+    fetch(`${SWAPI_URL}/films/`)
+        .then(response => response.json())
+        .then(apiData => {
+            apiData.results.forEach(element => {
+                createLI(movies, element.title);
+            });
+            movieData = apiData;
+        })
+        .catch(error => console.log(error));
+}
+
+const fetchShips = () => {
+    fetch(`${SWAPI_URL}/starships/`)
+        .then(response => response.json())
+        .then(apiData => {
+            apiData.results.forEach(element => {
+                createLI(ships, element.name);
+            });
+            shipData = apiData;
+        })
+        .catch(error => console.log(error));
+}
+
+const fetchCharacters = () => {
+    fetch(`${SWAPI_URL}/people/`)
+        .then(response => response.json())
+        .then(apiData => {
+            apiData.results.forEach(element => {
+                createLI(characters, element.name);
+            });
+            characterData = apiData;
+        })
+        .catch(error => console.log(error));
 }
 
 const callSWAPI = function () {
-    fetch("https://swapi.co/api/films")
-    .then(response => response.json())
-    .then(apiData => {
-        apiData.results.forEach(element => {
-            createLI(element.title);
-        });
-        movieData = apiData;
-    })
-    .catch(error => console.log(error));
+    fetchMovies();
+    fetchShips();
+    fetchCharacters();
 }
+    
 
 callSWAPI();
 
